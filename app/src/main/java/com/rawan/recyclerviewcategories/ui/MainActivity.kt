f7aa2a31500ec2db4d65091b5ffb7d9810614bb4
basma <basmaafifi08@gmail.com>
2021-09-16 17:31:16 -0700

    rr

--- app/src/main/java/com/rawan/recyclerviewcategories/ui/MainActivity.kt
+++ app/src/main/java/com/rawan/recyclerviewcategories/ui/MainActivity.kt
@@ -4,43 +4,62 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.rawan.recyclerviewcategories.R
+import com.rawan.recyclerviewcategories.databinding.ActivityMainBinding
 import com.rawan.recyclerviewcategories.model.Category
 import com.rawan.recyclerviewcategories.ui.adapter.OnCategoryClick
 import com.rawan.recyclerviewcategories.ui.adapter.RecyclerAdapter
 
 
-class MainActivity : AppCompatActivity(),OnCategoryClick {
+class MainActivity : AppCompatActivity(), OnCategoryClick {
 
     // lateinit  var adapter:RecyclerView.Adapter<RecyclerAdapter.UserViewHolder>
-    //lateinit var layoutManager:RecyclerView.LayoutManager
-    lateinit var recyclerView: RecyclerView
+    lateinit var layoutManager: RecyclerView.LayoutManager
+   // lateinit var recyclerView: RecyclerView
+    //var data = ArrayList<Category>()
     //var name: String? = null
-
+lateinit var binding: ActivityMainBinding
 
     val recyclerAdapter: RecyclerAdapter by lazy {
         RecyclerAdapter()
     }
 
     var categoryList: ArrayList<Category> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        recyclerView=findViewById(R.id.recyclerView)
-        recyclerView.layoutManager= GridLayoutManager(this,2)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+       // recyclerView = findViewById(R.id.recyclerView)
+        /* recyclerView.layoutManager = GridLayoutManager(this, 2)
+         recyclerView = RecyclerView(this)*/
 
-        //layoutManager= GridLayoutManager(this,2)
-        // recyclerView.layoutManager=layoutManager
+
+
+        for (i in 0..3) {
+            when (i % 4) {
+                0 -> categoryList.add(Category("orthodontics ", R.drawable.img1))
+                1 -> categoryList.add(Category("Endodontics", R.drawable.img2))
+                2 -> categoryList.add(Category("Dental Public Health", R.drawable.img3))
+                else -> categoryList.add(Category("Oral and Maxillofacial Surgery.", R.drawable.img))
+            }
+            recyclerAdapter.setList(categoryList)
+
+        }
+
+        layoutManager = GridLayoutManager(this, 2)
+        binding.recyclerView.layoutManager = layoutManager
 
 
         // adapter=RecyclerAdapter()
         //recyclerView.adapter=adapter
 
-        recyclerView.adapter=RecyclerAdapter()
+        binding.recyclerView.adapter = recyclerAdapter
 
 
-        recyclerAdapter.onCategoryClick=this
+        recyclerAdapter.onCategoryClick = this
 
     }
 
